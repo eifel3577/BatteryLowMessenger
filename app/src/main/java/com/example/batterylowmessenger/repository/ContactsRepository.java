bbdb629644c2d41884b063030ee8cf151a894b8b
eifel3577 <43643444+eifel3577@users.noreply.github.com>
2019-03-08 21:14:21 +0200

    Update ContactsRepository.java

--- app/src/main/java/com/example/batterylowmessenger/repository/ContactsRepository.java
+++ app/src/main/java/com/example/batterylowmessenger/repository/ContactsRepository.java
@@ -20,21 +20,30 @@ import java.util.concurrent.Executors;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+//создается в единственном экземпляре
 @Singleton
 public class ContactsRepository {
-
+    
+    // с помощью даггера получение экзекьютора для работы в отдельных потоках
     @Inject
     Executor executor;
+    // с помощью даггера получение контекста
     @Inject
     Context context;
+    //ссылка на БД
     private MyDatabase database;
+    //ссылка на интерфейс работы с БД
     private ContactDao contactDao;
 
 
+    //будет предоставляться как зависимость в даггер
     @Inject
     public ContactsRepository() {
+        //просит даггер предоставить нужные зависимости для данного репозитория
         App.getAppComponent().inject(this);
+        //инициализация БД
         database = App.getInstance().getDatabase();
+        //инициализация интерфейса работы с БД
         contactDao = database.contactDao();
     }
 
@@ -42,69 +51,86 @@ public class ContactsRepository {
      * @param fromDataBase - грузить данные из сети или из базы данных
      * @param callback - колбэк на ViewModel,куда будет идти результат*/
     public void getContact(final boolean fromDataBase, @NonNull final LoadData.LoadContactCallback callback){
-
+        //экзекьютор выполняет код в отдельном потоке
         executor.execute(new Runnable() {
             @Override
             public void run() {
                 if(!fromDataBase) {
+                    //получает все контакты из БД
                     List<Contact> contactList = contactDao.loadAll();
+                    //если БД вернула список хотя бы с одним контактом
                     if (contactList.size() > 0) {
+                        //то список отдается в колбек и метод заканчивается
                         callback.onContactsLoaded(contactList);
                         return;
                     }
                 }
-
-                        ContentResolver contentResolver = context.getContentResolver();
+                
+                    //ContentResolver это обьект поставщика контента.Поставщик контента управляет доступом к центральному
+                    //репозиторию данных ОС телефона (контакты,смс,другие пользовательские данные).Поставщик контента предоставляет
+                    //данные внешним приложениям
+                    //в виде одной или нескольких таблиц, аналогичных таблицам в реляционной базе данных. 
+                    ContentResolver contentResolver = context.getContentResolver();
+                    //получает список контактов телефона в алфавитном порядке
                     Cursor cursorContentResolver = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
 
                     try {
+                        //если в телефоне есть хоть один контакт
                         if (cursorContentResolver.getCount() > 0) {
-
+                            //проходит по контактам и на каждом шаге    
                             while (cursorContentResolver.moveToNext()) {
+                                //создает обьект Contact
                                 final Contact contact = new Contact();
+                                //получает от поставщика контента номер телефона контакта
                                 int hasPhoneNumber = Integer.parseInt(cursorContentResolver.getString(cursorContentResolver.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
-
+                                //если у контакта есть телефонный номер
                                 if (hasPhoneNumber > 0) {
+                                    //получает ИД контакта и присваивает обьекту
                                     String id = cursorContentResolver.getString(cursorContentResolver.getColumnIndex(ContactsContract.Contacts._ID));
                                     contact.setContactID(id);
-
+                                    //получает имя контакта и присваивает обьекту
                                     String name = cursorContentResolver.getString(cursorContentResolver.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
                                     contact.setContactName(name);
 
-
+                                    
                                     Cursor phoneCursor = contentResolver.query(
                                             ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                                             null,
                                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
                                             new String[]{id},
                                             null);
-
+                                    //получает номер телефона
                                     if (phoneCursor.moveToNext()) {
                                         String phone = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
                                         contact.setContactNumber(phone);
                                     }
-
+                                    //ставит контакт как неотмеченный
                                     contact.setChecked(false);
-
+                                    //работа с БД должна производится в отдельном потоке
                                     executor.execute(new Runnable() {
                                         @Override
                                         public void run() {
+                                            //сохраняет контакт в БД
                                             contactDao.save(contact);
                                         }
                                     });
-
+                                    //закрывает курсор
                                     phoneCursor.close();
                                 }
                             }
                         }
                     } finally {
+                        //после всего предыдущего в блоке try в любом случае
                         if (cursorContentResolver != null) {
+                            //закрывает поставщик контента
                             cursorContentResolver.close();
                             executor.execute(new Runnable() {
                                 @Override
                                 public void run() {
+                                    //вытягивает список контактов из БД в колбек
                                     callback.onContactsLoaded(contactDao.loadAll());
+                                    //очищает БД
                                     contactDao.clearTable();
                                 }
                             });
