a4c219e8784749ab8baa2d32dd3f15fcc9cf8f83
eifel3577 <43643444+eifel3577@users.noreply.github.com>
2019-03-08 21:09:23 +0200

    Update LoadData.java

--- app/src/main/java/com/example/batterylowmessenger/LoadData.java
+++ app/src/main/java/com/example/batterylowmessenger/LoadData.java
@@ -5,12 +5,13 @@ import com.example.batterylowmessenger.data.Contact;
 
 import java.util.List;
 
+//интерфейс колбек для возврата контактов из БД
 public interface LoadData {
 
     interface LoadContactCallback {
-
+        //БД вернула список контактов
         void onContactsLoaded(List<Contact> tasks);
-
+        //в БД 
         void onDataNotAvailable();
     }
 }
