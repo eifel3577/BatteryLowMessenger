1ca511669cfb8c2ff200934ce9b444672fc9612f
eifel3577 <43643444+eifel3577@users.noreply.github.com>
2019-03-08 20:23:05 +0200

    Update CheckedContact.java

--- app/src/main/java/com/example/batterylowmessenger/CheckedContact.java
+++ app/src/main/java/com/example/batterylowmessenger/CheckedContact.java
@@ -3,6 +3,7 @@ package com.example.batterylowmessenger;
 
 import java.util.List;
 
+//интерфейс коллбек для загрузки отмеченных контактов из БД
 public interface CheckedContact {
 
     interface CheckedContactCallback {
