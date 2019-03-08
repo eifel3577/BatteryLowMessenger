93b09bcefc00632faaec1dcbdd52da1ab1f5f0e6
eifel3577 <43643444+eifel3577@users.noreply.github.com>
2019-03-08 21:14:04 +0200

    Update HomeFragmentViewModel.java

--- app/src/main/java/com/example/batterylowmessenger/viewModels/HomeFragmentViewModel.java
+++ app/src/main/java/com/example/batterylowmessenger/viewModels/HomeFragmentViewModel.java
@@ -14,23 +14,29 @@ import com.example.batterylowmessenger.repository.ContactsRepository;
 
 import javax.inject.Inject;
 
+//viewModel для работы с HomeFragment
 public class HomeFragmentViewModel extends ViewModel {
 
+    //получение репозитория ContactsRepository через даггер
     @Inject
     ContactsRepository repository;
 
+    //просит даггер предоставить нужные зависимости для данного фрагмента
     public HomeFragmentViewModel() {
         App.getAppComponent().inject(this);
     }
 
+    //создание трансляции boolean-значения, ечть ли отмеченные контакты
     MutableLiveData<Boolean> isContactChecked = new MutableLiveData<>();
 
-
+    //возвращает трансляцию boolean-значения, ечть ли отмеченные контакты
     public LiveData<Boolean> getIsContactChecked(){
         return isContactChecked;
     }
 
+    //вызывается когда фрагмент HomeFragment появляется на экране
     public void start() {
+        //запрашивает репозиторий есть ли отмеченные контакты и отдает полученный булев в трансляцию isContactChecked
         repository.getCheckedList(new CheckedContact.CheckedContactCallback() {
             @Override
             public void onContactsLoaded(boolean resultCheck) {
